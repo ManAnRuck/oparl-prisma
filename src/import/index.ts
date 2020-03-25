a0649de2ab0a1435f86cc4796d289fd3dbbf6bbb
Manuel Ruck <github.com@manuelruck.de>
2020-03-25 08:39:21 +0100

    trigger set issue
    Signed-off-by: Manuel Ruck <github.com@manuelruck.de>

--- src/import/index.ts
+++ src/import/index.ts
@@ -21,33 +21,35 @@ export const startImport = async () => {
   if (organizationList) {
     let hasNext = true;
     do {
-      // const organizations = organizationList.data.map(organization => {
-      const organization = organizationList.data[0];
-      console.log('import orga');
-      console.log(organization.post);
-      const organizationPrepared = {
-        ...organization,
-        body: undefined,
-        'STERNBERG:gruppierung': undefined,
-        'STERNBERG:sortierung': undefined,
-        membership: undefined,
-        meeting: undefined,
-        location: undefined,
-        keyword: undefined,
-        subOrganizationOf: undefined,
-        post: {
-          set: organization.post,
-        },
-      };
-      return prisma.organization
-        .upsert({
-          create: organizationPrepared,
-          update: organizationPrepared,
-          where: { id: organizationPrepared.id },
-        })
-        .catch(console.log);
-      // });
-      if (organizationList?.next && false) {
+      const organizations = organizationList.data.map(organization => {
+        console.log('import orga');
+        console.log(organization.post);
+        const organizationPrepared:
+          | OrganizationCreateInput
+          | OrganizationCreateInput = {
+          ...organization,
+          body: undefined,
+          'STERNBERG:gruppierung': undefined,
+          'STERNBERG:sortierung': undefined,
+          membership: undefined,
+          meeting: undefined,
+          location: undefined,
+          keyword: undefined,
+          subOrganizationOf: undefined,
+          // post: organization.post,
+          post: {
+            set: organization.post,
+          },
+        };
+        return prisma.organization
+          .upsert({
+            create: organizationPrepared,
+            update: organizationPrepared,
+            where: { id: organizationPrepared.id },
+          })
+          .catch(console.log);
+      });
+      if (organizationList?.next) {
         organizationList = await organizationList.next();
       } else {
         hasNext = false;
